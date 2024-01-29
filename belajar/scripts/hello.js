c65bb172de2c3804936c286ba0a96e4bf237502c
nandoperd <nandoperd@gmail.com>
2024-01-29 13:58:39 +0700

    first commit

---
+++ belajar/scripts/hello.js
@@ -0,0 +1,9 @@
+export function hello(nama) { // menggunakan export untuk mengekspor fungsi module
+    console.info(`Hello ${nama}`)
+}
+
+export function hai(nama) {
+    console.info(`Hai ${nama}`)
+}
+
+export const nama = "Broker"
---
+++ belajar/scripts/tanpaModul.js
@@ -0,0 +1,9 @@
+function hello(nama) {
+    console.info(`Hello ${nama}`)
+}
+
+function hai(nama) {
+    console.info(`Hai ${nama}`)
+}
+
+const nama = "Broker"
